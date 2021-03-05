b7585c880aff87b710b4c7ebf9c882b4ee0fb227
Milan <milanvpage@gmail.com>
2021-03-04 22:37:39 -0800

    commit tree b5b063e714d80dc15ec08efb787c358e0f0cba49

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,42 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    #last_item_array = []
+
+    #array.join(,)
+    #array[-1].pop
+
+   # last_item_array = array[-1]
+    #last_item_array.join(" and ")
+
+    #array.join(" , ") << last_item_array.join(" and ")
+
+#array.join(" , ")
+
+    
+
+
+
+    
+    if array.length == 1
+         array.join(" , ")
+
+    elsif array.length == 2
+         array.join(" and ")
+
+    elsif array.length == 3
+        #array[-1].pop
+         #array.join(" , ") << " and #{array[-1]}"
+         array[-1].prepend "and "
+         array.join(", ")
+    
+         #array[-1] = array.pop
+    else array.length >= 3
+        #array[-1].pop
+        #array.join(" , ") << " and #{array[-1]}"
+         
+    
+        array[-1].prepend "and "
+         array.join(", ")
+    end
 end
+
